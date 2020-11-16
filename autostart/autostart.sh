539176cca11a87267a8923d9acd01cc934444a2c
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2020-11-16 15:57:55 +0100

    Create autostart.sh

---
+++ autostart/autostart.sh
@@ -0,0 +1,13 @@
+cd /
+mosquitto -d
+
+cd /home/pi/Documents/Python
+python3 ping.py &
+python3 arduino2meross.py &
+python3 beamer.py &
+python3 elements_webscrape.py &
+
+#cd /home/pi/Documents/Node-Js
+#nohup node blynk-test.js &
+sleep 5
+cd /
