087c0c1d8747ab7633592d4945f03a5aad97f1d1
Florian Fuchs <58077515+Floplosion05@users.noreply.github.com>
2020-11-19 21:10:01 +0100

    Update ping.py

--- Python/ping.py
+++ Python/ping.py
@@ -4,9 +4,12 @@ import threading
 import requests
 
 def send_message():
+    print('Client unreachable')
     requests.get(<<YOUR URL HERE>>)
 
 def subscribe(topic='002', host='localhost', port=1883, keepalive=60):
+    
+    global timer
     
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
@@ -19,6 +22,7 @@ def subscribe(topic='002', host='localhost', port=1883, keepalive=60):
             if msg.payload == b'pinghost':
                 publish.single('002', 'pingclient', hostname='localhost')
                 timer.cancel()
+                timer = threading.Timer(400, send_message)
                 timer.start()
     client = mqtt.Client()
     client.on_connect = on_connect
